e060f1dc5d5a60bc98873d6b884ae857d1b48ae8
venkasas <122641014+venkasas@users.noreply.github.com>
2023-12-03 18:16:29 -0500

    Add files via upload

---
+++ Assign2F2023/Controllers/ImageController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assign2F2023.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        
+    }
+}
